a27ba24624c626bdc50bc8c0f94824544dfe2d65
BigData <worker.opentext@gmail.com>
2025-01-08 22:01:19 +0200

    testing

--- index.ts
+++ index.ts
@@ -3,11 +3,12 @@ import { downloadFromPatreon, getPosts } from './utils/patreonUtils';
 
 const postUrl = 'https://www.patreon.com/c/kobeon/posts';
 
-// Download the post from patreon
-// downloadFromPatreon(postUrl, process.env.PATREON_OUTPUT_DIR || "patreonPosts");
-
 // fetching posts from patreon and save it to json file
 getPosts(postUrl).then(posts => {
     // save posts to a json file
     saveJsonToFile(posts, 'posts.json');
 }).catch(console.error);
+
+
+// Download the post from patreon
+// downloadFromPatreon(postUrl, process.env.PATREON_OUTPUT_DIR || "patreonPosts");
