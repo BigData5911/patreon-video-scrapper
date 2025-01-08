d2eb2743ec8d89f3bf17c4e0e37c77bb18fe3489
BigData <worker.opentext@gmail.com>
2025-01-08 21:51:56 +0200

    update file utils

--- index.ts
+++ index.ts
@@ -1,6 +1,12 @@
-import PatreonDownloader from 'patreon-dl';
-import { downloadFromPatreon } from './utils/patreolUtils';
+import { saveJsonToFile } from './utils/file';
+import { downloadFromPatreon, getPosts } from './utils/patreolUtils';
 
 const postUrl = 'https://www.patreon.com/c/kobeon/posts';
 
-downloadFromPatreon(postUrl, process.env.PATREON_OUTPUT_DIR || "patreonPosts");
+// downloadFromPatreon(postUrl, process.env.PATREON_OUTPUT_DIR || "patreonPosts");
+
+// fetching posts from patreon and save it to json file
+getPosts(postUrl).then(posts => {
+    // save posts to a json file
+    saveJsonToFile(posts, 'posts.json');
+}).catch(console.error);
